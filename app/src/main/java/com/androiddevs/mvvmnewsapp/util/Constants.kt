eec196538a51e574d066f416fd54156008ef8890
Rick Cherubini <richard.cherubini1@gmail.com>
2020-09-11 10:46:00 -0400

    Created Pagination. Tested Application. Finished App.

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val  API_KEY = "6d12a23c1e574ab79cd46edc3483c4e8"
         const val  BASE_URL = "https://newsapi.org"
         const val  SEARCH_NEWS_TIME_DELAY = 500L
+        const val  QUERY_PAGE_SIZE = 20
     }
 }
